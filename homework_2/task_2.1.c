b69f9c64ffb904f5c5e38cbf92d9bc9fcfb0cbe2
Alexander-Volkov-144 <70965603+Alexander-Volkov-144@users.noreply.github.com>
2020-09-25 01:54:56 +0300

    v1

---
+++ homework_2/task_2.1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <mem.h>
+
+int next(int* sol, int num, int sum)
+{
+    if (sum <= 0) {
+        for(int j = num; sol[j] != 0; j++){
+            sol[num] = 0;
+        }
+        int i = 0;
+        for (; sol[i + 1] != 0; i++) {
+            printf("%d + ", sol[i]);
+        }
+        printf("%d\n", sol[i]);
+    } else {
+        for (int i = sol[num - 1]; i > 0; i--) {
+            if (sum - i >= 0) {
+                sol[num] = i;
+                next(sol, num + 1, sum - i);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int sol[n + 1];
+    memset(sol, 0, (n + 1) * sizeof(int));
+    for (int i = n - 1; i != 0; i--) {
+        sol[0] = i;
+        next(sol, 1, n - i);
+    }
+
+    return 0;
+}
