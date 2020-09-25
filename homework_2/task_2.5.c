10be58096712b99bcf5fad3de2b16c2458526e6a
Alexander-Volkov-144 <70965603+Alexander-Volkov-144@users.noreply.github.com>
2020-09-25 21:29:01 +0300

    newfunc

--- homework_2/task_2.5.c
+++ homework_2/task_2.5.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 
-int factorial(int x)
+int factorialRecursive(int x)
 {
-    if (x < 1)
+    if (x <= 1)
         return 1;
-    return x * factorial(x - 1);
+    return x * factorialRecursive(x - 1);
 }
 
-int main()
+int factorialIterative(int x)
 {
-    int x;
-    scanf("%d", &x);
     int res = 1;
     for (int i = 2; i <= x; ++i) {
         res *= i;
     }
-    printf("%d \n%d", res, factorial(x));
+    return res;
+}
+
+int main()
+{
+    int x = 0;
+    printf("enter a natural number\n");
+    scanf("%d", &x);
+    printf("factorial of number %d, calculated iteratively = %d\n", x, factorialIterative(x));
+    printf("factorial of number %d, calculated recursively = %d", x, factorialRecursive(x));
     return 0;
 }
