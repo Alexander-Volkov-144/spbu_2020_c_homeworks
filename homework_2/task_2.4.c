046c4100eacc511789becab18ac97070535e9e1e
Alexander-Volkov-144 <70965603+Alexander-Volkov-144@users.noreply.github.com>
2020-09-27 17:26:54 +0300

    2newfunc

--- homework_2/task_2.4.c
+++ homework_2/task_2.4.c
@@ -1,40 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int maxSize = 20;  // 20 - максимальное количество цифр в числе, которое может быть записано в  unsigned long long
+
+int fillArray(int* array, unsigned long long x)
+{
+    int n = 0;
+    while (x > 0) {
+        array[n] = x % 10; // в массив записываются цифры числа
+        n += 1;
+        x /= 10;
+    }
+    return n; // возвращается количество элементов в массиве
+}
 
 void swap(int* x, int* y) // функция, меняющая местами два элемента массива
 {
     int tmp = *x;
     *x = *y;
     *y = tmp;
-    return;
+}
+
+int compare(const void* x, const void* y) // функция, сравнивающая два элемента массива для qsort
+{
+    return *(int*) x - *(int*) y;
+}
+
+void zeroMove(int* array)
+{
+    int i = 1;
+    while (array[i] == 0){
+        ++i;
+    }
+    swap(&array[0], &array[i]);
 }
 
 int main()
 {
-    int numbers[10]; // 10 - максимальное количество цифр в числе, которое может быть записано в  int
-    int x = 0;
+    int numbers[maxSize];
+    for (int i = 0; i < maxSize; ++i) {
+        numbers[i] = 0;
+    }
+    unsigned long long x = 0;
     printf("enter a natural number\n");
     scanf("%d", &x);
-    int n = 0;
-    while (x > 0) {
-        numbers[n] = x % 10; // в массив записываются цифры числа
-        n += 1;
-        x /= 10;
-    }
-    for (int i = 1; i < n; ++i) {
-        for (int j = 0; j < n - i; ++j) {
-            if (numbers[j] > numbers[j + 1]) {
-                swap(&numbers[j], &numbers[j + 1]); // массив сортируется по неубыванию
-            }
-        }
-    }
-    if (numbers[0] == 0) { // если на первом месте оказался 0, то нужно заменить его на цифру > 0
-        for (int i = 1;; ++i) {
-            if (numbers[i] != 0) {
-                swap(&numbers[0], &numbers[i]);
-                break;
-            }
-        }
-    }
+    int n = fillArray(numbers, x);
+    qsort(numbers, n, sizeof(int), compare);
+    if (numbers[0] == 0)
+        zeroMove(numbers);
     for (int i = 0; i < n; ++i) {
         printf("%d", numbers[i]);
     }
