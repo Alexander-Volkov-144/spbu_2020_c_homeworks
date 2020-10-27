1102763aac128a11062d1c154b3e2036258922bb
Alexander-Volkov-144 <70965603+Alexander-Volkov-144@users.noreply.github.com>
2020-10-27 13:39:16 +0300

    copied

---
+++ library/bst/bst.h
@@ -0,0 +1,17 @@
+#ifndef SPBU_2020_C_HOMEWORKS_BST_H
+#define SPBU_2020_C_HOMEWORKS_BST_H
+
+#include <stdbool.h>
+
+typedef struct BinarySearchTree BinarySearchTree;
+
+BinarySearchTree *createTree();
+void removeTree(BinarySearchTree *tree);
+
+bool exists(BinarySearchTree *tree, int value);
+bool addValue(BinarySearchTree *tree, int value);
+bool removeValue(BinarySearchTree *tree, int value);
+
+void printSymmetrical(BinarySearchTree *tree);
+
+#endif //SPBU_2020_C_HOMEWORKS_BST_H
