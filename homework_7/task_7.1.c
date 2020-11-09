91ffe820c36c5acd9a5104855e12633537843f9d
Alexander-Volkov-144 <70965603+Alexander-Volkov-144@users.noreply.github.com>
2020-11-09 10:11:29 +0300

    Update task_7.1.c

--- homework_7/task_7.1.c
+++ homework_7/task_7.1.c
@@ -4,48 +4,46 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main()
+Edge** getNumbers(int numberOfStudents)
 {
-    printf("enter the number of students (not including the first three)\n");
-    int numberOfStudents = 0;
-    scanf("%d", &numberOfStudents);
     Edge** students = createArrayOfEdges(numberOfStudents);
-    int number = 0, copiedFrom = 0;
     printf("enter %d student numbers and numbers of students which they copied\n", numberOfStudents);
+    int number = 0, copiedFrom = 0;
     for (int i = 0; i < numberOfStudents; ++i) {
         scanf("%d%d", &number, &copiedFrom);
         if (-1 == copiedFrom)
             copiedFrom = 0;
         students[i] = createEdge(copiedFrom, number, 1, true);
     }
-    Graph* copiedWorks = createGraph(numberOfStudents, numberOfStudents + 4, students);
+    return students;
+}
+
+int* findOriginal(Graph* copiedWorks, int numberOfStudents)
+{
     int* worksSources = (int*)malloc((numberOfStudents + 4) * sizeof(int));
     memset(worksSources, 0, (numberOfStudents + 4) * sizeof(int));
     bool* isCopied = (bool*)malloc((numberOfStudents + 4) * sizeof(bool));
-    memset(isCopied, false, (numberOfStudents + 4) * sizeof(bool));
-    getAllVertexesConnectedToGiven(copiedWorks, 1, isCopied);
-    for (int i = 0; i < numberOfStudents + 4; ++i) {
-        if (true == isCopied[i])
-            worksSources[i] = 1;
+    for (int i = 0; i < 4; ++i) {
+        memset(isCopied, false, (numberOfStudents + 4) * sizeof(bool));
+        getAllVertexesConnectedToGiven(copiedWorks, i, isCopied);
+        for (int j = 0; j < numberOfStudents + 4; ++j) {
+            if (true == isCopied[j])
+                worksSources[j] = i;
+        }
     }
-    memset(isCopied, false, (numberOfStudents + 4) * sizeof(bool));
-    getAllVertexesConnectedToGiven(copiedWorks, 2, isCopied);
-    for (int i = 0; i < numberOfStudents + 4; ++i) {
-        if (true == isCopied[i])
-            worksSources[i] = 2;
-    }
-    memset(isCopied, false, (numberOfStudents + 4) * sizeof(bool));
-    getAllVertexesConnectedToGiven(copiedWorks, 3, isCopied);
-    for (int i = 0; i < numberOfStudents + 4; ++i) {
-        if (true == isCopied[i])
-            worksSources[i] = 3;
-    }
-    memset(isCopied, false, (numberOfStudents + 4) * sizeof(bool));
-    getAllVertexesConnectedToGiven(copiedWorks, 0, isCopied);
-    for (int i = 0; i < numberOfStudents + 4; ++i) {
-        if (true == isCopied[i])
-            worksSources[i] = 0;
-    }
+    free(isCopied);
+    return worksSources;
+}
+
+
+int main()
+{
+    printf("enter the number of students (not including the first three)\n");
+    int numberOfStudents = 0;
+    scanf("%d", &numberOfStudents);
+    Edge** students = getNumbers(numberOfStudents);
+    Graph* copiedWorks = createGraph(numberOfStudents, numberOfStudents + 4, students);
+    int* worksSources = findOriginal(copiedWorks, numberOfStudents);
     for (int i = 1; i < 4; ++i) {
         printf("%d: original work\n", i);
     }
@@ -55,9 +53,8 @@ int main()
         else
             printf("%d: copied from %d\n", i, worksSources[i]);
     }
-    free(isCopied);
     free(worksSources);
     removeGraph(copiedWorks);
-    removeArrayOfEdges(students);
+    removeArrayOfEdges(students, numberOfStudents);
     return 0;
 }
