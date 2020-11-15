db674aa0ce7dbbeaab96a434296a973564e2811f
Alexander-Volkov-144 <70965603+Alexander-Volkov-144@users.noreply.github.com>
2020-11-15 18:05:27 +0300

    new library func

--- library/fileProcessing/fileProcessing.c
+++ library/fileProcessing/fileProcessing.c
@@ -1,30 +1,10 @@
 #include "fileProcessing.h"
+#include "../stringManipulations/stringManipulations.h"
 #include <ctype.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-bool isLetter(char character)
-{
-    return 'a' <= character && character <= 'z' || 'A' <= character && character <= 'Z';
-}
-
-char* doubleSize(char* oldString, int* oldSize)
-{
-    char* newString = (char*)malloc(*oldSize * 2 * sizeof(char));
-    if (newString == NULL) {
-        printf("error in allocating memory\n");
-        return NULL;
-    }
-    memset(newString, 0, *oldSize * 2 * sizeof(char));
-    for (int i = 0; i < *oldSize; ++i) {
-        newString[i] = oldString[i];
-    }
-    free(oldString);
-    *oldSize *= 2;
-    return newString;
-}
 
 char* readWordFromFile(FILE* file)
 {
---
+++ library/stringManipulations/stringManipulations.c
@@ -0,0 +1,26 @@
+#include "stringManipulations.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+bool isLetter(char character)
+{
+    return 'a' <= character && character <= 'z' || 'A' <= character && character <= 'Z';
+}
+
+char* doubleSize(char* oldString, int* oldSize)
+{
+    char* newString = (char*)malloc(*oldSize * 2 * sizeof(char));
+    if (newString == NULL) {
+        printf("error in allocating memory\n");
+        return NULL;
+    }
+    memset(newString, 0, *oldSize * 2 * sizeof(char));
+    for (int i = 0; i < *oldSize; ++i) {
+        newString[i] = oldString[i];
+    }
+    free(oldString);
+    *oldSize *= 2;
+    return newString;
+}
