e7d048a2f49b9c81e60dae0afa2aeb854c04d459
Alexander-Volkov-144 <70965603+Alexander-Volkov-144@users.noreply.github.com>
2020-12-15 12:50:54 +0300

    style fix 2

--- library/twoDimensionalArray/twoDimensionalArray.c
+++ library/twoDimensionalArray/twoDimensionalArray.c
@@ -48,8 +48,7 @@ void printSaddlePoints(TDArray* array)
             }
         }
     }
-
-
+    
     int currentMaximum = array->array[0][0];
     for (int j = 0; j < array->columnCount; ++j) { // внешний цикл по j, чтобы сохранить привычный порядок индексов i, j при обращении к массиву
         currentMaximum = array->array[0][j];
